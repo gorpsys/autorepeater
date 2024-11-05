a11beecda1704ebe82561ba3e70d8b728d596e2e
gorpsys <79201964+gorpsys@users.noreply.github.com>
2024-11-05 12:25:00 +0300

    fix codestyle, add debug mod without post order

--- client.py
+++ client.py
@@ -11,79 +11,90 @@ from tinkoff.invest import OrderType
 TOKEN = os.environ["INVEST_TOKEN"]
 DST_MONEY_RESERVED = 0.005
 
+
+def money_to_string(money):
+    """convert money to human-readable string"""
+    result = money.currency
+    result += ' - ' + str(money.units + money.nano / 1000000000.)
+    return result
+
+
+def blocked_to_string(blocked):
+    """convert blocked money to human-readable string"""
+    result = 'blocked ' + money_to_string(blocked)
+    return result
+
+
+def no_money_to_string(share):
+    """convert no money instrument to human-readable string"""
+    result = share.name + '(' + share.ticker + ')'
+    return result
+
+
+def currency_to_float(position):
+    """convert position price value to float"""
+    result = (position.current_price.units + position.current_price.nano /
+              1000000000.) * (position.quantity.units +
+                              position.quantity.nano / 1000000000.)
+    return result
+
+
+def currency_to_string(position):
+    """convert position price value to human-readable string"""
+    result = position.current_price.currency
+    result += ' - ' + str(currency_to_float(position))
+    return result
+
+
+def get_quantity_position(position):
+    """get quantity from position"""
+    return position.quantity.units + position.quantity.nano / 1000000000.
+
+
 class AutoRepeater:
     """Main class for automatically repeating operations of one account over another account."""
 
     def __init__(self, client):
         self.client = client
 
-    def money_to_string(self, money):
-        """convert money to human-readable string"""
-        result = money.currency
-        result += ' - ' + str(money.units + money.nano / 1000000000.)
-        return result
-
-    def blocked_to_string(self, blocked):
-        """convert blocked money to human-readable string"""
-        result = 'blocked ' + self.money_to_string(blocked)
-        return result
-
-    def no_money_to_string(self, share):
-        """convert no money instrument to human-readable string"""
-        result = share.name + '(' + share.ticker + ')'
-        return result
-
-    def currency_to_float(self, position):
-        """convert position price value to float"""
-        result = (position.current_price.units + position.current_price.nano /
-                  1000000000.) * (position.quantity.units +
-                                  position.quantity.nano / 1000000000.)
-        return result
-
-    def currency_to_string(self, position):
-        """convert position price value to human-readable string"""
-        result = position.current_price.currency
-        result += ' - ' + str(self.currency_to_float(position))
-        return result
-
-    def get_quantity_position(self, position):
-        """get quantity from position"""
-        return position.quantity.units + position.quantity.nano / 1000000000.
-
     def postiton_to_string(self, position):
+        """convert position to human-readable string"""
         if position.instrument_type == 'currency':
-            return self.currency_to_string(position)
-        elif position.instrument_type == 'share' or position.instrument_type == 'etf':
+            return currency_to_string(position)
+        if position.instrument_type in ['share', 'etf']:
             instrument = self.get_instrument(position.instrument_uid)
-            return self.no_money_to_string(instrument) + ' - ' + str(
-                self.get_quantity_position(
-                    position)) + ' - ' + self.currency_to_string(position)
-        else:
-            return str(position)
+            return no_money_to_string(instrument) + ' - ' + str(
+                get_quantity_position(position)) + ' - ' + currency_to_string(
+                    position)
+        return str(position)
 
-    def get_instrument(self, id):
-        result = self.client.instruments.find_instrument(query=id).instruments
+    def get_instrument(self, instrument_id):
+        """get instrument by instrument id"""
+        result = self.client.instruments.find_instrument(query=instrument_id).instruments
         if len(result) == 1:
             return result[0]
         raise Exception('error get instrument')
 
     def print_portfolio_by_account(self, account):
+        """print detailed information about account"""
         print(account.name + ' (' + str(account.id) + ')')
         print('------------')
         portfolio = self.client.operations.get_portfolio(account_id=account.id)
         total = 0.0
         for position in portfolio.positions:
             print(self.postiton_to_string(position))
-            total += self.currency_to_float(position)
+            total += currency_to_float(position)
         print('total: ' + str(total))
         print('============')
 
     def print_all_portfolio(self):
+        """print detailed information about all accounts"""
         response = self.client.users.get_accounts()
         for account in response.accounts:
             self.print_portfolio_by_account(account)
 
-    def sync_accounts(self, src_account_id, dst_account_id):
+    def sync_accounts(self, src_account_id, dst_account_id, debug):
+        """sync positions from src account to dst account"""
         print("src account")
         portfolio_src = self.client.operations.get_portfolio(
             account_id=src_account_id)
@@ -93,7 +104,7 @@ class AutoRepeater:
             print(self.postiton_to_string(position))
             if position.instrument_type != 'currency':
                 src_positions[position.instrument_uid] = position
-                total_src += self.currency_to_float(position)
+                total_src += currency_to_float(position)
         print('total: ' + str(total_src))
 
         print("dst account")
@@ -105,94 +116,96 @@ class AutoRepeater:
             print(self.postiton_to_string(position))
             if position.instrument_type != 'currency':
                 dst_positions[position.instrument_uid] = position
-            total_dst += self.currency_to_float(position)
+            total_dst += currency_to_float(position)
         total_dst = total_dst * (1 - DST_MONEY_RESERVED)
         print('total: ' + str(total_dst))
 
         ratio = total_dst / total_src
         target_positions = {}
 
-        for instrument_id in src_positions.keys():
-            position = src_positions[instrument_id]
-            target_positions[instrument_id] = ratio * \
-                self.get_quantity_position(position)
+        for item_id, item_value in src_positions.items():
+            target_positions[item_id] = ratio * \
+                get_quantity_position(item_value)
 
-        for instrument_id in dst_positions.keys():
+        for item_id, item_value in dst_positions.items():
             instrument = self.client.instruments.get_instrument_by(
                 id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_UID,
-                id=instrument_id).instrument
-            position = dst_positions[instrument_id]
-            if instrument_id not in target_positions.keys():
+                id=item_id).instrument
+            if item_id not in target_positions:
                 quantity = round(
-                    self.get_quantity_position(position) / instrument.lot)
+                    get_quantity_position(item_value) / instrument.lot)
                 if quantity > 0:
-                    print('Продать: ' + self.no_money_to_string(instrument) +
-                          ' ' + str(quantity) + ' лотов')
-                    print(
-                        self.client.orders.post_order(
-                            instrument_id=instrument_id,
-                            quantity=quantity,
-                            direction=OrderDirection.ORDER_DIRECTION_SELL,
-                            account_id=dst_account_id,
-                            order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
-                    )
-            elif target_positions[instrument_id] < self.get_quantity_position(
-                    position):
+                    print('Продать: ' + no_money_to_string(instrument) + ' ' +
+                          str(quantity) + ' лотов')
+                    if not debug:
+                        print(
+                            self.client.orders.post_order(
+                                instrument_id=item_id,
+                                quantity=quantity,
+                                direction=OrderDirection.ORDER_DIRECTION_SELL,
+                                account_id=dst_account_id,
+                                order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
+                        )
+            elif target_positions[item_id] < get_quantity_position(
+                    item_value):
                 quantity = round(
-                    (self.get_quantity_position(position) -
-                     target_positions[instrument_id]) / instrument.lot)
+                    (get_quantity_position(item_value) -
+                     target_positions[item_id]) / instrument.lot)
                 if quantity > 0:
-                    print('Продать: ' + self.no_money_to_string(instrument) +
-                          ' ' + str(quantity) + ' лотов')
-                    print(
-                        self.client.orders.post_order(
-                            instrument_id=instrument_id,
-                            quantity=quantity,
-                            direction=OrderDirection.ORDER_DIRECTION_SELL,
-                            account_id=dst_account_id,
-                            order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
-                    )
+                    print('Продать: ' + no_money_to_string(instrument) + ' ' +
+                          str(quantity) + ' лотов')
+                    if not debug:
+                        print(
+                            self.client.orders.post_order(
+                                instrument_id=item_id,
+                                quantity=quantity,
+                                direction=OrderDirection.ORDER_DIRECTION_SELL,
+                                account_id=dst_account_id,
+                                order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
+                        )
 
-        for instrument_id in target_positions.keys():
+        for item_id, item_value in target_positions.items():
             instrument = self.client.instruments.get_instrument_by(
                 id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_UID,
-                id=instrument_id).instrument
-            if instrument_id not in dst_positions.keys():
-                position = src_positions[instrument_id]
-                quantity = round(target_positions[instrument_id] /
+                id=item_id).instrument
+            if item_id not in dst_positions:
+                position = src_positions[item_id]
+                quantity = round(item_value /
                                  instrument.lot)
                 if quantity > 0:
-                    print('Купить: ' + self.no_money_to_string(instrument) + ' ' +
+                    print('Купить: ' + no_money_to_string(instrument) + ' ' +
                           str(quantity) + ' лотов')
-                    print(
-                        self.client.orders.post_order(
-                            instrument_id=instrument_id,
-                            quantity=quantity,
-                            direction=OrderDirection.ORDER_DIRECTION_BUY,
-                            account_id=dst_account_id,
-                            order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
-                    )
-            elif target_positions[instrument_id] > self.get_quantity_position(
-                    dst_positions[instrument_id]):
-                position = dst_positions[instrument_id]
+                    if not debug:
+                        print(
+                            self.client.orders.post_order(
+                                instrument_id=item_id,
+                                quantity=quantity,
+                                direction=OrderDirection.ORDER_DIRECTION_BUY,
+                                account_id=dst_account_id,
+                                order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
+                        )
+            elif item_value > get_quantity_position(
+                    dst_positions[item_id]):
+                position = dst_positions[item_id]
                 quantity = round(
-                    (target_positions[instrument_id] -
-                     self.get_quantity_position(position)) / instrument.lot)
+                    (item_value -
+                     get_quantity_position(position)) / instrument.lot)
                 if quantity > 0:
-                    print('Купить: ' + self.no_money_to_string(instrument) + ' ' +
+                    print('Купить: ' + no_money_to_string(instrument) + ' ' +
                           str(quantity) + ' лотов')
-                    print(
-                        self.client.orders.post_order(
-                            instrument_id=instrument_id,
-                            quantity=quantity,
-                            direction=OrderDirection.ORDER_DIRECTION_BUY,
-                            account_id=dst_account_id,
-                            order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
-                    )
+                    if not debug:
+                        print(
+                            self.client.orders.post_order(
+                                instrument_id=item_id,
+                                quantity=quantity,
+                                direction=OrderDirection.ORDER_DIRECTION_BUY,
+                                account_id=dst_account_id,
+                                order_type=OrderType.ORDER_TYPE_BESTPRICE).order_id
+                        )
 
-    def mainflow(self, src, dst):
-
-        self.sync_accounts(src, dst)
+    def mainflow(self, src, dst, debug):
+        """sync accounts when changing"""
+        self.sync_accounts(src, dst, debug)
 
         for response in self.client.operations_stream.positions_stream(
                 accounts=[src, dst]):
@@ -204,19 +217,21 @@ class AutoRepeater:
                       and len(response.position.securities) == 0
                       and response.position.money[0].blocked_value.units == 0
                       and response.position.money[0].blocked_value.nano == 0):
-                self.sync_accounts(src, dst)
+                self.sync_accounts(src, dst, debug)
 
 
 def main():
+    """main function"""
     parser = argparse.ArgumentParser(description="autorepeater")
 
     with Client(token=TOKEN, target=INVEST_GRPC_API) as client:
         autorepeater = AutoRepeater(client)
         autorepeater.print_all_portfolio()
+        parser.add_argument("--debug", type=bool, help="режим отладки")
         parser.add_argument("src", help="id счёта источника")
         parser.add_argument("dst", help="id счёта назначения")
         args = parser.parse_args()
-        autorepeater.mainflow(args.src, args.dst)
+        autorepeater.mainflow(args.src, args.dst, args.debug)
 
 
 if __name__ == "__main__":
