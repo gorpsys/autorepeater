ff54fb82990eada748c4559df1d265373348d7ef
gorpsys <79201964+gorpsys@users.noreply.github.com>
2025-03-10 19:07:13 +0300

    add app.py

---
+++ app/app.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+
+from autorepeater import Runner
+from autorepeater import RunnerParams
+
+def main():
+    """main function"""
+    parser = argparse.ArgumentParser(description="autorepeater")
+
+    parser.add_argument("--debug", action='store_true', help="режим отладки")
+    parser.add_argument("-s", "--src", type=str, help="id счёта источника")
+    parser.add_argument("-d", "--dst", type=str, help="id счёта назначения")
+    parser.add_argument("-t", "--threshold", type=float, help="порог стоимости, ниже "
+                        "которого не выполняется синхронизация - доля стоимости счёта"
+                        " назначения. По умолчанию 0.001")
+    parser.add_argument("-r", "--reserve", type=float, help="резев на счёте назначения"
+                        " для округлений и комиссий. Доля стоимости счёта назначения. "
+                        "По умолчания 0.005")
+    args = parser.parse_args()
+
+    invest_token = os.environ["INVEST_TOKEN"]
+
+    runer = Runner(
+        token=invest_token,
+        src=args.src,
+        dst=args.dst,
+        params=RunnerParams(
+            debug=args.debug,
+            threshold=args.threshold,
+            reserve=args.reserve))
+    runer.run()
+
+if __name__ == "__main__":
+    main()
